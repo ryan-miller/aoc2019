835225505de9b1251425b20b99547bda28c171b4
Ryan Miller <rmiller@fastly.com>
2019-12-09 09:31:00 -0700

    day2 part one

--- day1_test.go
+++ day1_test.go
@@ -1,174 +1,172 @@
 package aoc2019
 
-import "testing"
+// func TestSum(t *testing.T) {
 
-func TestSum(t *testing.T) {
+// 	sumOfMasses := 3427947
+// 	recursiveSumOfMasses := 5139037
+// 	masses := []int{
+// 		125860,
+// 		66059,
+// 		147392,
+// 		64447,
+// 		72807,
+// 		136018,
+// 		144626,
+// 		68233,
+// 		130576,
+// 		92645,
+// 		52805,
+// 		79642,
+// 		74361,
+// 		98270,
+// 		110796,
+// 		62578,
+// 		58421,
+// 		125079,
+// 		52683,
+// 		144885,
+// 		148484,
+// 		113638,
+// 		125026,
+// 		112534,
+// 		125479,
+// 		51539,
+// 		122007,
+// 		60048,
+// 		67923,
+// 		76115,
+// 		144822,
+// 		115991,
+// 		133505,
+// 		85249,
+// 		142441,
+// 		90211,
+// 		87022,
+// 		68196,
+// 		117577,
+// 		58112,
+// 		116865,
+// 		108253,
+// 		127674,
+// 		93302,
+// 		58817,
+// 		126794,
+// 		89824,
+// 		134386,
+// 		99700,
+// 		125855,
+// 		119753,
+// 		64456,
+// 		68167,
+// 		88047,
+// 		127864,
+// 		146890,
+// 		71912,
+// 		128375,
+// 		134365,
+// 		91544,
+// 		104179,
+// 		84700,
+// 		95937,
+// 		78409,
+// 		94604,
+// 		130423,
+// 		98348,
+// 		87489,
+// 		105103,
+// 		94794,
+// 		123723,
+// 		134298,
+// 		88283,
+// 		59543,
+// 		53645,
+// 		89325,
+// 		109301,
+// 		143668,
+// 		96250,
+// 		130371,
+// 		140436,
+// 		95857,
+// 		98543,
+// 		91372,
+// 		137056,
+// 		142578,
+// 		116185,
+// 		96588,
+// 		93025,
+// 		122275,
+// 		99201,
+// 		110492,
+// 		109700,
+// 		106755,
+// 		120979,
+// 		60957,
+// 		134983,
+// 		130840,
+// 		132329,
+// 		65057,
+// 	}
 
-	sumOfMasses := 3427947
-	recursiveSumOfMasses := 5139037
-	masses := []int{
-		125860,
-		66059,
-		147392,
-		64447,
-		72807,
-		136018,
-		144626,
-		68233,
-		130576,
-		92645,
-		52805,
-		79642,
-		74361,
-		98270,
-		110796,
-		62578,
-		58421,
-		125079,
-		52683,
-		144885,
-		148484,
-		113638,
-		125026,
-		112534,
-		125479,
-		51539,
-		122007,
-		60048,
-		67923,
-		76115,
-		144822,
-		115991,
-		133505,
-		85249,
-		142441,
-		90211,
-		87022,
-		68196,
-		117577,
-		58112,
-		116865,
-		108253,
-		127674,
-		93302,
-		58817,
-		126794,
-		89824,
-		134386,
-		99700,
-		125855,
-		119753,
-		64456,
-		68167,
-		88047,
-		127864,
-		146890,
-		71912,
-		128375,
-		134365,
-		91544,
-		104179,
-		84700,
-		95937,
-		78409,
-		94604,
-		130423,
-		98348,
-		87489,
-		105103,
-		94794,
-		123723,
-		134298,
-		88283,
-		59543,
-		53645,
-		89325,
-		109301,
-		143668,
-		96250,
-		130371,
-		140436,
-		95857,
-		98543,
-		91372,
-		137056,
-		142578,
-		116185,
-		96588,
-		93025,
-		122275,
-		99201,
-		110492,
-		109700,
-		106755,
-		120979,
-		60957,
-		134983,
-		130840,
-		132329,
-		65057,
-	}
+// 	 t.Run("get fuel need for single module", func(t *testing.T) {
 
-	t.Run("get fuel need for single module", func(t *testing.T) {
+// 	// 	sumTests := []struct {
+// 	// 		mass    int
+// 	// 		reqFuel int
+// 	// 	}{
+// 	// 		{12, 2},
+// 	// 		{14, 2},
+// 	// 		{1969, 654},
+// 	// 		{100756, 33583},
+// 	// 		{125860, 41951},
+// 	// 	}
 
-		sumTests := []struct {
-			mass    int
-			reqFuel int
-		}{
-			{12, 2},
-			{14, 2},
-			{1969, 654},
-			{100756, 33583},
-			{125860, 41951},
-		}
+// 	// 	for _, tt := range sumTests {
+// 	// 		got := Sum(tt.mass)
+// 	// 		assertSumMatch(t, got, tt.reqFuel)
 
-		for _, tt := range sumTests {
-			got := Sum(tt.mass)
-			assertSumMatch(t, got, tt.reqFuel)
+// 	// 	}
 
-		}
+// 	// })
 
-	})
+// 	// t.Run("get fuel need for many modules", func(t *testing.T) {
 
-	t.Run("get fuel need for many modules", func(t *testing.T) {
+// 	// 	got := SumAll(masses)
+// 	// 	want := sumOfMasses
 
-		got := SumAll(masses)
-		want := sumOfMasses
+// 	// 	assertSumMatch(t, got, want)
 
-		assertSumMatch(t, got, want)
+// 	// })
 
-	})
+// 	// t.Run("get recursive fuel need", func(t *testing.T) {
 
-	t.Run("get recursive fuel need", func(t *testing.T) {
+// 	// 	sumPlusTests := []struct {
+// 	// 		mass    int
+// 	// 		totFuel int
+// 	// 	}{
+// 	// 		{14, 2},
+// 	// 		{1969, 966},
+// 	// 		{100756, 50346},
+// 	// 	}
 
-		sumPlusTests := []struct {
-			mass    int
-			totFuel int
-		}{
-			{14, 2},
-			{1969, 966},
-			{100756, 50346},
-		}
+// 	// 	for _, tt := range sumPlusTests {
+// 	// 		got := RecursiveSum(tt.mass)
+// 	// 		assertSumMatch(t, got, tt.totFuel)
+// 	// 	}
 
-		for _, tt := range sumPlusTests {
-			got := RecursiveSum(tt.mass)
-			assertSumMatch(t, got, tt.totFuel)
-		}
+// 	// })
 
-	})
+// 	// t.Run("get recursive fuel need for many modules", func(t *testing.T) {
 
-	t.Run("get recursive fuel need for many modules", func(t *testing.T) {
+// 	// 	got := RecursiveSumAll(masses)
+// 	// 	want := recursiveSumOfMasses
 
-		got := RecursiveSumAll(masses)
-		want := recursiveSumOfMasses
+// 	// 	assertSumMatch(t, got, want)
+// 	// })
 
-		assertSumMatch(t, got, want)
-	})
+// }
 
-}
-
-func assertSumMatch(t *testing.T, got, want int) {
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
+// func assertSumMatch(t *testing.T, got, want int) {
+// 	if got != want {
+// 		t.Errorf("got %d want %d", got, want)
+// 	}
+// }
--- day2_test.go
+++ day2_test.go
@@ -1,7 +1,8 @@
 package aoc2019
 
 import (
-	"reflect"
+	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -27,84 +28,114 @@ if a is 99 stop.
 
 func TestStringToSlice(t *testing.T) {
 
-	intCode := "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,5,19,23,1,23,5,27,2,27,10,31,1,5,31,35,2,35,6,39,1,6,39,43,2,13,43,47,2,9,47,51,1,6,51,55,1,55,9,59,2,6,59,63,1,5,63,67,2,67,13,71,1,9,71,75,1,75,9,79,2,79,10,83,1,6,83,87,1,5,87,91,1,6,91,95,1,95,13,99,1,10,99,103,2,6,103,107,1,107,5,111,1,111,13,115,1,115,13,119,1,13,119,123,2,123,13,127,1,127,6,131,1,131,9,135,1,5,135,139,2,139,6,143,2,6,143,147,1,5,147,151,1,151,2,155,1,9,155,0,99,2,14,0,0"
-	finalCode := "490668,0,0,2,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,0,1,5,19,1,1,23,5,2,2,27,10,8,1,5,31,9,2,35,6,18,1,6,39,20,2,13,43,100,2,9,47,300,1,6,51,302,1,55,9,305,2,6,59,610,1,5,63,611,2,67,13,3055,1,9,71,3058,1,75,9,3061,2,79,10,12244,1,6,83,12246,1,5,87,12,247,1,6,91,12249,1,95,13,12254,1,10,99,12258,2,6,103,24516,1,107,5,24517,1,111,13,24522,1,115,13,24527,1,13,119,24532,2,123,13,122660,1,127,6,122662,1,131,9,122665,1,5,135,122666,2,139,6,245332,2,6,143,490664,1,5,147,490665,1,151,2,490665,1,9,155,0,99,2,14,0,0"
-	t.Run("convert commas separated string to slice of integers", func(t *testing.T) {
-		testStrings := []struct {
-			s        string
-			intSlice []int
-		}{
-			{"1,9,10,3", []int{1, 9, 10, 3}},
-			{"2,3,11,0", []int{2, 3, 11, 0}},
-			{"99", []int{99}},
-			{"30,40,50", []int{30, 40, 50}},
-		}
+	//intCode := "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,5,19,23,1,23,5,27,2,27,10,31,1,5,31,35,2,35,6,39,1,6,39,43,2,13,43,47,2,9,47,51,1,6,51,55,1,55,9,59,2,6,59,63,1,5,63,67,2,67,13,71,1,9,71,75,1,75,9,79,2,79,10,83,1,6,83,87,1,5,87,91,1,6,91,95,1,95,13,99,1,10,99,103,2,6,103,107,1,107,5,111,1,111,13,115,1,115,13,119,1,13,119,123,2,123,13,127,1,127,6,131,1,131,9,135,1,5,135,139,2,139,6,143,2,6,143,147,1,5,147,151,1,151,2,155,1,9,155,0,99,2,14,0,0"
+	finalCode := "9706670,12,2,2,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,48,1,5,19,49,1,23,5,50,2,27,10,200,1,5,31,201,2,35,6,402,1,6,39,404,2,13,43,2020,2,9,47,6060,1,6,51,6062,1,55,9,6065,2,6,59,12130,1,5,63,12131,2,67,13,60655,1,9,71,60658,1,75,9,60661,2,79,10,242644,1,6,83,242646,1,5,87,242647,1,6,91,242649,1,95,13,242654,1,10,99,242658,2,6,103,485316,1,107,5,485317,1,111,13,485322,1,115,13,485327,1,13,119,485332,2,123,13,2426660,1,127,6,2426662,1,131,9,2426665,1,5,135,2426666,2,139,6,4853332,2,6,143,9706664,1,5,147,9706665,1,151,2,9706667,1,9,155,0,99,2,14,0,0"
 
-		for _, ts := range testStrings {
-			got := ConvertStringToSlice(ts.s)
-			want := ts.intSlice
+	t.Run("find nouns and verbs", func(t *testing.T) {
 
-			if !reflect.DeepEqual(got, want) {
-				t.Errorf("want %v got %v", want, got)
+		finalCodeSlice := ConvertStringToSlice(finalCode)
+
+		//opCodePosition := 0
+		stanza := ""
+		counter := 0
+		for i := 0; i < len(finalCodeSlice); i++ {
+
+			currentValue := strconv.Itoa(finalCodeSlice[i])
+
+			stanza += currentValue + ","
+			if counter == 0 && currentValue == "99" {
+				fmt.Printf("%v\n", stanza)
+				return
 			}
-		}
+			counter++
 
-	})
-
-	t.Run("convert integer slice to comma separated string", func(t *testing.T) {
-		testStrings := []struct {
-			intSlice []int
-			s        string
-		}{
-			{[]int{1, 9, 10, 3}, "1,9,10,3"},
-			{[]int{2, 3, 11, 0}, "2,3,11,0"},
-			{[]int{99}, "99"},
-			{[]int{30, 40, 50}, "30,40,50"},
-		}
-
-		for _, ts := range testStrings {
-			got := ConvertSliceToString(ts.intSlice)
-			want := ts.s
-
-			if !reflect.DeepEqual(got, want) {
-				t.Errorf("want %v got %v", want, got)
+			if counter%4 == 0 {
+				fmt.Printf("%v\n", stanza)
+				stanza = ""
+				counter = 0
 			}
+			//
+
 		}
 
 	})
 
-	t.Run("find opCodes", func(t *testing.T) {
-		testString := "1,9,10,3,2,3,11,0,99,30,40,50"
-		want := []int{1, 2, 99}
+	// t.Run("convert commas separated string to slice of integers", func(t *testing.T) {
+	// 	testStrings := []struct {
+	// 		s        string
+	// 		intSlice []int
+	// 	}{
+	// 		{"1,9,10,3", []int{1, 9, 10, 3}},
+	// 		{"2,3,11,0", []int{2, 3, 11, 0}},
+	// 		{"99", []int{99}},
+	// 		{"30,40,50", []int{30, 40, 50}},
+	// 	}
 
-		got := FindOpCodes(testString)
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("want %v got %v", want, got)
-		}
-	})
+	// 	for _, ts := range testStrings {
+	// 		got := ConvertStringToSlice(ts.s)
+	// 		want := ts.intSlice
 
-	t.Run("process intCodes", func(t *testing.T) {
-		testIntCodes := []struct {
-			Initial string
-			Final   string
-		}{
-			{"99", "99"},
-			{"1,0,0,0,99", "2,0,0,0,99"},
-			{"2,3,0,3,99", "2,3,0,6,99"},
-			{"2,4,4,5,99,0", "2,4,4,5,99,9801"},
-			{"1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99"},
-			{intCode, finalCode},
-		}
+	// 		if !reflect.DeepEqual(got, want) {
+	// 			t.Errorf("want %v got %v", want, got)
+	// 		}
+	// 	}
 
-		for _, ts := range testIntCodes {
-			got := ProcessIntCode(ts.Initial)
-			want := ts.Final
+	// })
 
-			if got != want {
-				t.Errorf("want %v got %v", want, got)
-			}
-		}
-	})
+	// t.Run("convert integer slice to comma separated string", func(t *testing.T) {
+	// 	testStrings := []struct {
+	// 		intSlice []int
+	// 		s        string
+	// 	}{
+	// 		{[]int{1, 9, 10, 3}, "1,9,10,3"},
+	// 		{[]int{2, 3, 11, 0}, "2,3,11,0"},
+	// 		{[]int{99}, "99"},
+	// 		{[]int{30, 40, 50}, "30,40,50"},
+	// 	}
+
+	// 	for _, ts := range testStrings {
+	// 		got := ConvertSliceToString(ts.intSlice)
+	// 		want := ts.s
+
+	// 		if !reflect.DeepEqual(got, want) {
+	// 			t.Errorf("want %v got %v", want, got)
+	// 		}
+	// 	}
+
+	// })
+
+	// t.Run("find opCodes", func(t *testing.T) {
+	// 	testString := "1,9,10,3,2,3,11,0,99,30,40,50"
+	// 	want := []int{1, 2, 99}
+
+	// 	got := FindOpCodes(testString)
+	// 	if !reflect.DeepEqual(got, want) {
+	// 		t.Errorf("want %v got %v", want, got)
+	// 	}
+	// })
+
+	// t.Run("process intCodes", func(t *testing.T) {
+	// 	testIntCodes := []struct {
+	// 		Initial string
+	// 		Final   string
+	// 	}{
+	// 		{"99", "99"},
+	// 		{"1,0,0,0,99", "2,0,0,0,99"},
+	// 		{"2,3,0,3,99", "2,3,0,6,99"},
+	// 		{"2,4,4,5,99,0", "2,4,4,5,99,9801"},
+	// 		{"1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99"},
+	// 		{intCode, finalCode},
+	// 	}
+
+	// 	for _, ts := range testIntCodes {
+	// 		got := ProcessIntCode(ts.Initial)
+	// 		want := ts.Final
+
+	// 		if got != want {
+	// 			t.Errorf("want %v got %v", want, got)
+	// 		}
+	// 	}
+	// })
 
 }
 
